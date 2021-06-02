48bf5871329dae68ea9c8e31889535f7b9d9ced0
SevdaErkan <51799297+SevdaErkan@users.noreply.github.com>
2021-06-02 14:24:24 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Slider from "./components/Slider";
+import "./styles.scss";
+
+export default function App() {
+   return (
+      <div className="App">
+         <Slider />
+      </div>
+   );
+}
